f4f49c018b206b0e3f7b0951609418c675f094a3
Mark Corbin <markpc1608@gmail.com>
2024-10-19 01:28:34 -0400

    Initial Commit

---
+++ SwiftRecipe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftRecipe
+//
+//  Created by Mark Corbin on 2024-10-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftRecipe/SwiftRecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftRecipeApp.swift
+//  SwiftRecipe
+//
+//  Created by Mark Corbin on 2024-10-19.
+//
+
+import SwiftUI
+
+@main
+struct SwiftRecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
