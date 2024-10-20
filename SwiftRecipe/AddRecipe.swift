88c54bc61a247bba64c74453ad09a8dfc7803ab5
Mark Corbin <markpc1608@gmail.com>
2024-10-20 14:55:59 -0400

    started adding form and modifiers to addRecipe view

--- SwiftRecipe/AddRecipe.swift
+++ SwiftRecipe/AddRecipe.swift
@@ -8,8 +8,49 @@
 import SwiftUI
 
 struct AddRecipe: View {
+    @State private var title: String = ""
+    @State private var description: String = ""
+    @State private var ingredient: String = ""
+    @State private var ingredients: [String] = []
+    @State private var step: String = ""
+    @State private var steps: [String] = []
+    @State private var image: String = ""
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Add Recipe")
+
+        Form {
+            Section(header: Text("Add Recipe")) {
+                VStack {
+                    TextField("Title", text: $title)
+                    TextField("Description", text: $description)
+
+                    VStack(alignment: .leading) {
+                        TextField("Ingredient", text: $ingredient)
+                        Button(action: {
+                            if !ingredient.isEmpty {
+                                ingredients.append(ingredient)
+                                ingredient = ""
+                            }
+                        }) {
+                            Text("Add Ingredient")
+                        }
+                    }
+
+                    VStack(alignment: .leading) {
+                        TextField("Step", text: $step)
+                        Button(action: {
+                            if !step.isEmpty {
+                                steps.append(step)
+                                step = ""
+                            }
+                        }) {
+                            Text("Add Step")
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
