843cfa14369df72210b111852df1686dd1b87c93
Mark Corbin <markpc1608@gmail.com>
2024-10-23 13:23:33 -0400

    Added filter computed property to list of recipes screen

--- SwiftRecipe/ListOfRecipes.swift
+++ SwiftRecipe/ListOfRecipes.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ListOfRecipes: View {
     @State private var recipes = recipesArray
+    @State private var filterByName: String = ""
 
     func addRecipe(_ recipe: Recipe) {
         recipes.append(recipe)
@@ -18,72 +19,96 @@ struct ListOfRecipes: View {
         recipes = recipes.filter { $0.id != recipe.id }
     }
 
+    var filteredRecipes: [Recipe] {
+        if filterByName.isEmpty {
+            return recipes
+        } else {
+            return recipes
+                .filter { $0.Title.localizedStandardContains(filterByName) }
+        }
+    }
+
     var body: some View {
         VStack {
-            NavigationView {
-                VStack {
-                    List(recipes) {
-                        recipe in NavigationLink(
-                            destination: RecipeDetailView(
-                                recipeDetail: recipe,
-                                delete: {
-                                    deleteRecipe(recipe)
-                                }
-                            )
-                        ) {
-                            VStack(alignment: .leading, spacing: 20) {
-                                VStack(alignment: .leading) {
-                                    Text("Recipe:")
-                                        .font(.title)
-                                        .foregroundColor(.teal)
-                                    Text(recipe.Title)
-                                        .font(.title2)
-                                        .foregroundColor(.blue)
-                                }
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                                .fontWeight(.bold)
+            Form {
+                Section(header: Text("Filter Recipes")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(.teal)
 
-                                AsyncImage(url: URL(string: recipe.Image)) {
-                                    image in
-                                    image.resizable()
-                                        .frame(width: .infinity, height: 200)
-                                        .clipped()
-                                        .shadow(color: .gray, radius: 5, x: 0, y: 5)
-                                } placeholder: {
-                                    ProgressView()
-                                }
+                ) {
+                    TextField("Name", text: $filterByName)
+                }
+            }
+            .frame(height: 120)
+            .padding()
 
-                                VStack(alignment: .leading, spacing: 5) {
-                                    Text("Description:")
-                                        .font(.system(size: 22, weight: .medium))
-                                    Text(recipe.Description)
-                                        .font(.system(size: 18, weight: .regular))
+            VStack {
+                NavigationView {
+                    VStack {
+                        List(filteredRecipes) {
+                            recipe in NavigationLink(
+                                destination: RecipeDetailView(
+                                    recipeDetail: recipe,
+                                    delete: {
+                                        deleteRecipe(recipe)
+                                    }
+                                )
+                            ) {
+                                VStack(alignment: .leading, spacing: 20) {
+                                    VStack(alignment: .leading) {
+                                        Text("Recipe:")
+                                            .font(.title)
+                                            .foregroundColor(.teal)
+                                        Text(recipe.Title)
+                                            .font(.title2)
+                                            .foregroundColor(.blue)
+                                    }
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                    .fontWeight(.bold)
+
+                                    AsyncImage(url: URL(string: recipe.Image)) {
+                                        image in
+                                        image.resizable()
+                                            .frame(width: .infinity, height: 200)
+                                            .clipped()
+                                            .shadow(color: .gray, radius: 5, x: 0, y: 5)
+                                    } placeholder: {
+                                        ProgressView()
+                                    }
+
+                                    VStack(alignment: .leading, spacing: 5) {
+                                        Text("Description:")
+                                            .font(.system(size: 22, weight: .medium))
+                                        Text(recipe.Description)
+                                            .font(.system(size: 18, weight: .regular))
+                                    }
                                 }
                             }
-                        }
-                        .padding()
-                        .border(Color.black)
-                        .background(Color.white)
-                        .listRowSeparator(.hidden)
-                        .navigationTitle("Recipes")
-                    } // List End
-                    .listStyle(PlainListStyle())
+                            .padding()
+                            .border(Color.black)
+                            .background(Color.white)
+                            .listRowSeparator(.hidden)
+                            .navigationTitle("Recipes")
+                        } // List End
+                        .listStyle(PlainListStyle())
 
-                    VStack {
-                        NavigationLink(
-                            destination: AddRecipe(addRecipe: addRecipe)
-                        ) {
-                            Text("Add New Recipe")
-                                .font(.title2)
-                                .foregroundColor(.white)
-                                .padding()
-                                .background(Color.blue)
-                                .cornerRadius(10)
-                        }
-                    } //  Add Recipe End
-                } // Inside Navigation VStack End
-            } // Navigation End
-        } // Outside Navigation VStack End
+                        VStack {
+                            NavigationLink(
+                                destination: AddRecipe(addRecipe: addRecipe)
+                            ) {
+                                Text("Add New Recipe")
+                                    .font(.title2)
+                                    .foregroundColor(.white)
+                                    .padding()
+                                    .background(Color.blue)
+                                    .cornerRadius(10)
+                            }
+                        } //  Add Recipe VStack End
+                    } // Inside Navigation VStack End
+                } // Navigation End
+            } // Outside Navigation VStack End
+        } // First VStack end
     } // Body View End
 }
 
