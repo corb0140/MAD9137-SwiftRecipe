087119b953bec64cdef20451bd10ed8f6e3623c2
Mark Corbin <markpc1608@gmail.com>
2024-10-23 13:33:37 -0400

    removed a VStack from list of recipe screen, placed form inside VStack after NavigationView

--- SwiftRecipe/ListOfRecipes.swift
+++ SwiftRecipe/ListOfRecipes.swift
@@ -30,85 +30,83 @@ struct ListOfRecipes: View {
 
     var body: some View {
         VStack {
-            Form {
-                Section(header: Text("Filter Recipes")
-                    .font(.title)
-                    .fontWeight(.bold)
-                    .foregroundColor(.teal)
+            NavigationView {
+                VStack {
+                    Form {
+                        Section(header: Text("Filter Recipes")
+                            .font(.title)
+                            .fontWeight(.bold)
+                            .foregroundColor(.teal)
 
-                ) {
-                    TextField("Name", text: $filterByName)
-                }
-            }
-            .frame(height: 120)
-            .padding()
+                        ) {
+                            TextField("Name", text: $filterByName)
+                        }
+                    }
+                    .frame(height: 120)
+                    .padding()
+                    
+                    List(filteredRecipes) {
+                        recipe in NavigationLink(
+                            destination: RecipeDetailView(
+                                recipeDetail: recipe,
+                                delete: {
+                                    deleteRecipe(recipe)
+                                }
+                            )
+                        ) {
+                            VStack(alignment: .leading, spacing: 20) {
+                                VStack(alignment: .leading) {
+                                    Text("Recipe:")
+                                        .font(.title)
+                                        .foregroundColor(.teal)
+                                    Text(recipe.Title)
+                                        .font(.title2)
+                                        .foregroundColor(.blue)
+                                }
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .fontWeight(.bold)
 
-            VStack {
-                NavigationView {
-                    VStack {
-                        List(filteredRecipes) {
-                            recipe in NavigationLink(
-                                destination: RecipeDetailView(
-                                    recipeDetail: recipe,
-                                    delete: {
-                                        deleteRecipe(recipe)
-                                    }
-                                )
-                            ) {
-                                VStack(alignment: .leading, spacing: 20) {
-                                    VStack(alignment: .leading) {
-                                        Text("Recipe:")
-                                            .font(.title)
-                                            .foregroundColor(.teal)
-                                        Text(recipe.Title)
-                                            .font(.title2)
-                                            .foregroundColor(.blue)
-                                    }
-                                    .frame(maxWidth: .infinity, alignment: .leading)
-                                    .fontWeight(.bold)
+                                AsyncImage(url: URL(string: recipe.Image)) {
+                                    image in
+                                    image.resizable()
+                                        .frame(width: .infinity, height: 200)
+                                        .clipped()
+                                        .shadow(color: .gray, radius: 5, x: 0, y: 5)
+                                } placeholder: {
+                                    ProgressView()
+                                }
 
-                                    AsyncImage(url: URL(string: recipe.Image)) {
-                                        image in
-                                        image.resizable()
-                                            .frame(width: .infinity, height: 200)
-                                            .clipped()
-                                            .shadow(color: .gray, radius: 5, x: 0, y: 5)
-                                    } placeholder: {
-                                        ProgressView()
-                                    }
-
-                                    VStack(alignment: .leading, spacing: 5) {
-                                        Text("Description:")
-                                            .font(.system(size: 22, weight: .medium))
-                                        Text(recipe.Description)
-                                            .font(.system(size: 18, weight: .regular))
-                                    }
+                                VStack(alignment: .leading, spacing: 5) {
+                                    Text("Description:")
+                                        .font(.system(size: 22, weight: .medium))
+                                    Text(recipe.Description)
+                                        .font(.system(size: 18, weight: .regular))
                                 }
                             }
-                            .padding()
-                            .border(Color.black)
-                            .background(Color.white)
-                            .listRowSeparator(.hidden)
-                            .navigationTitle("Recipes")
-                        } // List End
-                        .listStyle(PlainListStyle())
+                        }
+                        .padding()
+                        .border(Color.black)
+                        .background(Color.white)
+                        .listRowSeparator(.hidden)
+                        .navigationTitle("Recipes")
+                    } // List End
+                    .listStyle(PlainListStyle())
 
-                        VStack {
-                            NavigationLink(
-                                destination: AddRecipe(addRecipe: addRecipe)
-                            ) {
-                                Text("Add New Recipe")
-                                    .font(.title2)
-                                    .foregroundColor(.white)
-                                    .padding()
-                                    .background(Color.blue)
-                                    .cornerRadius(10)
-                            }
-                        } //  Add Recipe VStack End
-                    } // Inside Navigation VStack End
-                } // Navigation End
-            } // Outside Navigation VStack End
-        } // First VStack end
+                    VStack {
+                        NavigationLink(
+                            destination: AddRecipe(addRecipe: addRecipe)
+                        ) {
+                            Text("Add New Recipe")
+                                .font(.title2)
+                                .foregroundColor(.white)
+                                .padding()
+                                .background(Color.blue)
+                                .cornerRadius(10)
+                        }
+                    } //  Add Recipe VStack End
+                } // Inside Navigation VStack End
+            } // Navigation End
+        } // Outside Navigation VStack End
     } // Body View End
 }
 
