dc2b441048169ebf41fbb7e6de7f410937a57ac7
Mark Corbin <markpc1608@gmail.com>
2024-10-24 14:43:38 -0400

    created edit recipe screen. added button edit recipe button to recipe detail. ccreated hstack for buttons. added add recipe func to recipe list

---
+++ SwiftRecipe/EditRecipe.swift
@@ -0,0 +1,18 @@
+//
+//  EditRecipe.swift
+//  SwiftRecipe
+//
+//  Created by Mark Corbin on 2024-10-24.
+//
+
+import SwiftUI
+
+struct EditRecipe: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditRecipe()
+}
