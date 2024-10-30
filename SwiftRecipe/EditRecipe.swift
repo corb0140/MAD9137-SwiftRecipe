0ed36791514ad5667082c84dbc23d7a252539991
Mark Corbin <markpc1608@gmail.com>
2024-10-30 06:37:46 -0400

    changed design for add and edit recipe. need to remove background that appears when I call add and edit in sheet

--- SwiftRecipe/EditRecipe.swift
+++ SwiftRecipe/EditRecipe.swift
@@ -23,184 +23,195 @@ struct EditRecipe: View {
     var updateRecipeDetail: (Recipe) -> Void
   
     var body: some View {
-        Form {
-            ScrollView {
-                Section(header: Text("Edit Recipe")
-                    .font(.title)
-                    .fontWeight(.bold)
-                    .foregroundColor(.teal)
-                ) {
-                    VStack(spacing: 25) {
-                        VStack(alignment: .leading, spacing: 15) {
-                            VStack(alignment: .leading, spacing: 5) {
-                                Text("Title:")
-                                    .font(.title3)
-                                    .foregroundColor(.gray)
-                                TextField("Title", text: $title)
-                            }
-                            
-                            VStack(alignment: .leading, spacing: 5) {
-                                Text("Description:")
-                                    .font(.title3)
-                                    .foregroundColor(.gray)
-                                TextField("Description", text: $description)
-                            }
-                        }
-                        
-                        // Ingredients
-                        VStack(alignment: .leading) {
-                            TextField("Ingredient", text: $ingredient)
-                                .padding(10)
-                                .overlay(
-                                    RoundedRectangle(cornerRadius: 5)
-                                        .stroke(Color.gray, lineWidth: 1)
-                                )
+        ZStack {
+            Rectangle()
+                .fill(Color.white)
+                .clipShape(
+                    RoundedTopLeftRight(radius: 40, corners: [.topLeft, .topRight])
+                )
+                .edgesIgnoringSafeArea(.bottom)
+                .shadow(radius: 10)
+            
+            VStack {
+                ScrollView {
+                    Section(header: Text("Edit Recipe")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(.teal)
+                    ) {
+                        VStack(spacing: 25) {
+                            VStack(alignment: .leading, spacing: 15) {
+                                VStack(alignment: .leading, spacing: 5) {
+                                    Text("Title:")
+                                        .font(.title3)
+                                        .foregroundColor(.gray)
+                                    TextField("Title", text: $title)
+                                }
                                 
-                            // See Ingredients Currently In Array
-                            ForEach(ingredients.indices, id: \.self) { index in
-                                HStack {
-                                    Text(ingredients[index])
-                                    
-                                    Spacer()
-                                    
-                                    // Remove Ingredient
-                                    Button(action: {
-                                        ingredients.remove(at: index)
-                                    }) {
-                                        Text("Remove").foregroundColor(.red)
+                                VStack(alignment: .leading, spacing: 5) {
+                                    Text("Description:")
+                                        .font(.title3)
+                                        .foregroundColor(.gray)
+                                    TextField("Description", text: $description)
+                                }
+                            }
+                            
+                            // Ingredients
+                            VStack(alignment: .leading) {
+                                TextField("Ingredient", text: $ingredient)
+                                    .padding(10)
+                                    .overlay(
+                                        RoundedRectangle(cornerRadius: 5)
+                                            .stroke(Color.gray, lineWidth: 1)
+                                    )
+                                
+                                // See Ingredients Currently In Array
+                                ForEach(ingredients.indices, id: \.self) { index in
+                                    HStack {
+                                        Text(ingredients[index])
+                                        
+                                        Spacer()
+                                        
+                                        // Remove Ingredient
+                                        Button(action: {
+                                            ingredients.remove(at: index)
+                                        }) {
+                                            Text("Remove").foregroundColor(.red)
+                                        }
                                     }
                                 }
-                            }
-                            
-                            // Add Ingredient
-                            Button(action: {
-                                if !ingredient.isEmpty {
-                                    ingredients.append(ingredient)
-                                    ingredient = ""
+                                
+                                // Add Ingredient
+                                Button(action: {
+                                    if !ingredient.isEmpty {
+                                        ingredients.append(ingredient)
+                                        ingredient = ""
+                                    }
+                                }) {
+                                    Text("Add Ingredient")
+                                        .foregroundColor(.white)
+                                        .padding(9)
                                 }
-                            }) {
-                                Text("Add Ingredient")
-                                    .foregroundColor(.white)
-                                    .padding(9)
+                                .background(Color.blue)
+                                .cornerRadius(5)
+                                .buttonStyle(BorderedButtonStyle())
                             }
-                            .background(Color.blue)
-                            .cornerRadius(5)
-                            .buttonStyle(BorderedButtonStyle())
-                        }
-                        
-                        // Step
-                        VStack(alignment: .leading) {
-                            TextField("Step", text: $step)
-                                .padding(10)
-                                .overlay(
-                                    RoundedRectangle(cornerRadius: 5)
-                                        .stroke(Color.gray, lineWidth: 1)
-                                )
                             
-                            // See Steps Currently In Array
-                            ForEach(steps.indices, id: \.self) { index in
-                                HStack {
-                                    Text(steps[index])
-                                    
-                                    Spacer()
-                                    
-                                    // Remove Step
-                                    Button(action: {
-                                        steps.remove(at: index)
-                                    }) {
-                                        Text("Remove").foregroundColor(.red)
+                            // Step
+                            VStack(alignment: .leading) {
+                                TextField("Step", text: $step)
+                                    .padding(10)
+                                    .overlay(
+                                        RoundedRectangle(cornerRadius: 5)
+                                            .stroke(Color.gray, lineWidth: 1)
+                                    )
+                                
+                                // See Steps Currently In Array
+                                ForEach(steps.indices, id: \.self) { index in
+                                    HStack {
+                                        Text(steps[index])
+                                        
+                                        Spacer()
+                                        
+                                        // Remove Step
+                                        Button(action: {
+                                            steps.remove(at: index)
+                                        }) {
+                                            Text("Remove").foregroundColor(.red)
+                                        }
                                     }
                                 }
+                                
+                                // Add Step
+                                Button(action: {
+                                    if !step.isEmpty {
+                                        steps.append(step)
+                                        step = ""
+                                    }
+                                }) {
+                                    Text("Add Step")
+                                        .foregroundColor(.white)
+                                        .padding(9)
+                                }
+                                .background(Color.blue)
+                                .cornerRadius(5)
+                                .buttonStyle(BorderedButtonStyle())
                             }
                             
-                            // Add Step
-                            Button(action: {
-                                if !step.isEmpty {
-                                    steps.append(step)
-                                    step = ""
+                            // Image
+                            VStack(alignment: .leading, spacing: 30) {
+                                TextField("Image Url", text: $imageUrl)
+                                
+                                if !imageUrl.isEmpty {
+                                    AsyncImage(url: URL(string: imageUrl)) {
+                                        image in
+                                        image.resizable()
+                                            .aspectRatio(contentMode: .fill)
+                                            .frame(width: .infinity, height: 200)
+                                            .clipped()
+                                            .shadow(color: .gray, radius: 5, x: 0, y: 5)
+                                    } placeholder: {
+                                        ProgressView()
+                                    }
+                                } else {
+                                    Text("No Image Provided")
                                 }
-                            }) {
-                                Text("Add Step")
-                                    .foregroundColor(.white)
-                                    .padding(9)
-                            }
-                            .background(Color.blue)
-                            .cornerRadius(5)
-                            .buttonStyle(BorderedButtonStyle())
-                        }
-                        
-                        // Image
-                        VStack(alignment: .leading, spacing: 30) {
-                            TextField("Image Url", text: $imageUrl)
-                            
-                            if !imageUrl.isEmpty {
-                                AsyncImage(url: URL(string: imageUrl)) {
-                                    image in
-                                    image.resizable()
-                                        .aspectRatio(contentMode: .fill)
-                                        .frame(width: .infinity, height: 200)
-                                        .clipped()
-                                        .shadow(color: .gray, radius: 5, x: 0, y: 5)
-                                } placeholder: {
-                                    ProgressView()
-                                }
-                            } else {
-                                Text("No Image Provided")
                             }
                         }
                     }
-                }
-                
-                HStack {
-                    Button(
-                        action: {
-                            let updatedRecipe = Recipe(
-                                id: recipeDetail.id,
-                                Title: title,
-                                Description: description,
-                                Ingredients: ingredients,
-                                Steps: steps,
-                                Image: imageUrl
-                            )
-                            
-                            updateRecipeDetail(updatedRecipe)
-                            editRecipe(updatedRecipe)
-                            showEditView = false
-                           
-                        }) {
-                            Text("Update Recipe")
-                                .font(.subheadline)
-                                .frame(width: 110)
-                                .padding(18)
-                                .foregroundColor(.white)
-                                .background(Color.blue)
-                                .cornerRadius(5)
-                        }
-                        .padding(.top, 8)
-                        .frame(width: .infinity)
+                    
+                    HStack {
+                        Button(
+                            action: {
+                                let updatedRecipe = Recipe(
+                                    id: recipeDetail.id,
+                                    Title: title,
+                                    Description: description,
+                                    Ingredients: ingredients,
+                                    Steps: steps,
+                                    Image: imageUrl
+                                )
+                                
+                                updateRecipeDetail(updatedRecipe)
+                                editRecipe(updatedRecipe)
+                                showEditView = false
+                                
+                            }) {
+                                Text("Update Recipe")
+                                    .font(.subheadline)
+                                    .frame(width: 110)
+                                    .padding(18)
+                                    .foregroundColor(.white)
+                                    .background(Color.blue)
+                                    .cornerRadius(5)
+                            }
+                            .padding(.top, 8)
+                            .frame(width: .infinity)
                         
-                    Button(
-                        action: {
-                            showEditView = false
-                        }) {
-                            Text("Cancel")
-                                .font(.subheadline)
-                                .frame(width: 110)
-                                .padding(18)
-                                .background(Color.red)
-                                .foregroundColor(.white)
-                                .cornerRadius(5)
-                        }
-                        .padding(.top, 8)
+                        Button(
+                            action: {
+                                showEditView = false
+                            }) {
+                                Text("Cancel")
+                                    .font(.subheadline)
+                                    .frame(width: 110)
+                                    .padding(18)
+                                    .background(Color.red)
+                                    .foregroundColor(.white)
+                                    .cornerRadius(5)
+                            }
+                            .padding(.top, 8)
+                    }
                 }
             }
-        }
-        .onAppear {
-            title = recipeDetail.Title
-            description = recipeDetail.Description
-            ingredients = recipeDetail.Ingredients
-            steps = recipeDetail.Steps
-            imageUrl = recipeDetail.Image
+            .onAppear {
+                title = recipeDetail.Title
+                description = recipeDetail.Description
+                ingredients = recipeDetail.Ingredients
+                steps = recipeDetail.Steps
+                imageUrl = recipeDetail.Image
+            }
+            .padding()
         }
     }
 }
